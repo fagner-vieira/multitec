56e46bfb076d6bd5527e6765c543c0b45a09621f
Fagner <contato@farovi.com.br>
2018-12-17 16:03:55 -0200

    Mult 01 - inicio da construção com Flexbox

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Multitec</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/css.css" rel="stylesheet">
+    </head>
+    <body>
+        <header class="container">
+            <section class="logo"><img src="images/logo.png" alt=""></section>
+        </header>
+    </body>
+</html>
