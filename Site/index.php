8dc2bc347910b87d4a58aea318e2f40eb188e8f1
Fagner <contato@farovi.com.br>
2018-12-30 23:25:46 -0200

    Mult 01 Novo - Topo e form

---
+++ Site/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/css.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
+
+		<div class="container-fluid">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 topo01">
+						<img src="images/logo.png" class="img-responsive center-block" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid bg01">
+			<div class="container">
+				<div class="row topo02">
+					<div class="col-lg-8 marginProd">
+						<div class="row">
+							<div class="col-lg-6">
+								<img src="images/produto.png" class="img-responsive" alt="">
+							</div>
+							<div class="col-lg-6 marginL">
+									<p class="txtOne">MultiUno</p>
+									<p class="txtOneB">Executiva</p>
+									<div class="row espaco">
+										<div class="col-lg-2 tiraM">
+											<p class="txtOneC">Por:<br/>10x</p>
+										</div>
+										<div class="col-lg-8 tiraM">
+												<p class="txtOneD">R$ 101</p>
+										</div>
+										<div class="col-lg-2 tiraM">
+											<p class="centavos">,74</p>
+										</div>
+									</div>
+									<p class="txtOneE">Ou R$ 969,00 à vista.</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-lg-4 twoProduct">
+						<p class="txtTwo">Garanta seu orçamento<br/>com desconto</p>
+                    	<p class="txtTwoB">Aproveite essa<br/>oportunidade incrível!</p>
+                    	<p class="formBox">
+                        	<form class="form-horizontal" name="form-gohotsale" id="form-gohotsale" method="POST" action="https://gohotsale.com.br/leads" novalidate>
+                            	<input type="hidden" name="hotsite" id="hotsite" value="casa-toda-favorita-hotpage">
+                            	<input type="hidden" name="company" id="company" value="favorita-mov-decor">
+                            	<fieldset class="col-md-12">
+                            	<?php if(isset($_GET['error']) && $_GET['error'] === 'profanity') { ?>
+                            	<div class="alert alert-danger" role="alert"> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> <span class="sr-only">Erro:</span> Seja mais educado! </div>
+                            	<?php } ?>
+                            	<?php if(isset($_GET['error']) && $_GET['error'] === 'repeat') { ?>
+                            	<div class="alert alert-danger" role="alert"> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> <span class="sr-only">Erro:</span> Verifique os seus dados e tente novamente! </div>
+                            	<?php } ?>
+                            	<!-- Text input-->
+									<div class="form-group"> 
+										<!--label class="col-md-4 control-label" for="nome">Nome</label-->
+										<div class="col-md-12">
+										<input id="name" name="name" type="text" data-required placeholder="Nome" class="form-control input-md formulario" value="<?php if(isset($_GET['name'])) { echo $_GET['name']; } ?>" required="required">
+										</div>
+									</div>
+									<!-- Text input-->
+									<div class="form-group"> 
+										<!--label class="col-md-4 control-label" for="email">E-mail</label-->
+										<div class="col-md-12">
+										<input id="email" name="email" type="email" data-required placeholder="E-mail" class="form-control input-md formulario" value="<?php if(isset($_GET['email'])) { echo $_GET['email']; } ?>" required="required">
+										</div>
+									</div>
+
+									<!-- Text input-->
+									<div class="form-group"> 
+										<!--label class="col-md-4 control-label" for="telefone">Telefone</label-->
+										<div class="col-md-12">
+										<input id="phone" name="phone" type="text" data-required required="required" placeholder="Telefone" value="<?php if(isset($_GET['phone'])) { echo $_GET['phone']; } ?>" class="form-control input-md formulario">
+										</div>
+									</div>
+									<!-- Button -->
+									<div class="form-group"> 
+										<!--label class="col-md-4 control-label" for="enviar"></label-->
+										<div class="col-md-12" style="text-align: center;">
+										<button id="enviar" name="enviar" class="hotEnviar">GARANTIR DESCONTO</button>
+										</div>
+									</div>
+									</fieldset>
+							</form>
+                    	</p>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	
+									
+
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-1.11.3.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="js/bootstrap.js"></script>
+  </body>
+</html>
